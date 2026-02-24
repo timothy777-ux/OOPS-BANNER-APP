82f9a087ab78de0c758c11b1e3192f0bb74362a7
Timothy Gnanaseelan <71440878+timothy777-ux@users.noreply.github.com>
2026-02-24 19:04:36 +0530

    new fearture uc5

--- src/com.oopsbanner/OOPSbannerapp.java
+++ src/com.oopsbanner/OOPSbannerapp.java
@@ -1,61 +1,54 @@
 public class OOPSbannerapp{
 
-    // Method to build letter O
-    public static String[] buildO() {
-        return new String[]{
-                String.join("", "   ***   "),
-                String.join("", "  *   *  "),
-                String.join("", " *     * "),
-                String.join("", " *     * "),
-                String.join("", " *     * "),
-                String.join("", "  *   *  "),
-                String.join("", "   ***   ")
-        };
-    }
-
-    // Method to build letter P
-    public static String[] buildP() {
-        return new String[]{
-                String.join("", " *****   "),
-                String.join("", " *    *  "),
-                String.join("", " *    *  "),
-                String.join("", " ******   "),
-                String.join("", " *       "),
-                String.join("", " *       "),
-                String.join("", " *       ")
-        };
-    }
-
-    // Method to build letter S
-    public static String[] buildS() {
-        return new String[]{
-                String.join("", "  *****  "),
-                String.join("", " *       "),
-                String.join("", " *       "),
-                String.join("", "  ****   "),
-                String.join("", "      *  "),
-                String.join("", "      *  "),
-                String.join("", " *****   ")
-        };
-    }
-
     public static void main(String[] args) {
 
-        // Populate letters in order O O P S
-        String[][] letters = { buildO(), buildO(), buildP(), buildS() };
+        // Inline declaration and initialization
+        String[] banner = {
+                String.join("  ",
+                        "   ***   ",
+                        "   ***   ",
+                        " *****   ",
+                        "  *****  "
+                ),
+                String.join("  ",
+                        "  *   *  ",
+                        "  *   *  ",
+                        " *    *  ",
+                        " *       "
+                ),
+                String.join("  ",
+                        " *     * ",
+                        " *     * ",
+                        " *    *  ",
+                        " *       "
+                ),
+                String.join("  ",
+                        " *     * ",
+                        " *     * ",
+                        " *****   ",
+                        "  ****   "
+                ),
+                String.join("  ",
+                        " *     * ",
+                        " *     * ",
+                        " *       ",
+                        "      *  "
+                ),
+                String.join("  ",
+                        "  *   *  ",
+                        "  *   *  ",
+                        " *       ",
+                        "      *  "
+                ),
+                String.join("  ",
+                        "   ***   ",
+                        "   ***   ",
+                        " *       ",
+                        " *****   "
+                )
+        };
 
-        String[] banner = new String[7];
-
-        // Combine rows
-        for (int i = 0; i < 7; i++) {
-            String line = "";
-            for (String[] letter : letters) {
-                line += letter[i] + "  ";
-            }
-            banner[i] = line;
-        }
-
-        // Print using enhanced for loop
+        // Print using for-each loop
         for (String line : banner) {
             System.out.println(line);
         }
